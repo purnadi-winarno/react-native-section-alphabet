29b32fd3c9eab732f199cc1f8de8b0740db94434
Purnadi <purnadi.winarno@automate-app.com>
2022-02-03 13:03:44 +0700

    add ts debounce package

--- src/index.tsx
+++ src/index.tsx
@@ -1,162 +1,138 @@
-import * as React from "react"
-import { SectionList, View, Text, SectionListData } from "react-native"
-import sectionListGetItemLayout from "react-native-section-list-get-item-layout"
-import getSectionData from "./utilities/getSectionData"
-import ListLetterIndex from "./components/ListLetterIndex"
-import IData from "./interfaces/IData"
-import ISectionData from "./interfaces/ISectionData"
-import IAlphabetListProps from "./interfaces/IAlphabetListProps"
-import styles from "./styles/AlphabetListStyle"
-import sizes from "./values/sizes"
+import * as React from "react";
+import { SectionList, View, Text, SectionListData } from "react-native";
+import sectionListGetItemLayout from "react-native-section-list-get-item-layout";
+import getSectionData from "./utilities/getSectionData";
+import ListLetterIndex from "./components/ListLetterIndex";
+import IData from "./interfaces/IData";
+import ISectionData from "./interfaces/ISectionData";
+import IAlphabetListProps from "./interfaces/IAlphabetListProps";
+import styles from "./styles/AlphabetListStyle";
+import sizes from "./values/sizes";
+import { debounce } from "ts-debounce";
 
-function debounce(func: Function, wait: number, immediate?: boolean) {
-  var timeout
-
-  return function () {
-    var context = this,
-      args = arguments
-    var later = function () {
-      timeout = null
-      if (!immediate) func.apply(context, args)
-    }
-    var callNow = immediate && !timeout
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
-    if (callNow) func.apply(context, args)
-  }
-}
 export default class AlphabetList extends React.PureComponent<IAlphabetListProps> {
   state: {
-    sectionData: ISectionData[]
-    optimizedData: ISectionData[]
-    alphabetSelectedIndex: number
-    selectedLetter: string
+    sectionData: ISectionData[];
+    optimizedData: ISectionData[];
+    alphabetSelectedIndex: number;
+    selectedLetter: string;
   } = {
     sectionData: [],
     optimizedData: [],
     alphabetSelectedIndex: 0,
     selectedLetter: "A",
-  }
+  };
 
-  sectionList!: SectionList
-  onGetItemLayout: any
+  sectionList!: SectionList;
+  onGetItemLayout: any;
 
   constructor(props: IAlphabetListProps) {
-    super(props)
+    super(props);
 
-    const {
-      getItemHeight: onGetItemHeight = () => sizes.itemHeight,
-      sectionHeaderHeight = sizes.itemHeight,
-    } = props
+    const { getItemHeight: onGetItemHeight = () => sizes.itemHeight, sectionHeaderHeight = sizes.itemHeight } = props;
 
     this.onGetItemLayout = sectionListGetItemLayout({
       getItemHeight: (_rowData: any, sectionIndex: number, rowIndex: number) =>
         onGetItemHeight({ sectionIndex, rowIndex }),
       getSectionHeaderHeight: () => sectionHeaderHeight,
       getSectionFooterHeight: () => 0,
-    })
+    });
   }
 
   componentDidMount() {
-    this.setSectionData()
+    this.setSectionData();
   }
 
   componentDidUpdate(prevProps: IAlphabetListProps) {
     if (prevProps.data.length !== this.props.data.length) {
-      this.setSectionData()
+      this.setSectionData();
     }
   }
 
   private setSectionData = () => {
-    const { data = [] } = this.props
-    const sectionData = getSectionData(data)
-    const self = this
+    const { data = [] } = this.props;
+    const sectionData = getSectionData(data);
+    const self = this;
 
     this.setState({ sectionData }, () => {
-      const optimizedData = sectionData?.filter(
-        (section) => section?.data?.length > 0,
-      )
+      const optimizedData = sectionData?.filter((section) => section?.data?.length > 0);
 
-      self.setState({ optimizedData })
-    })
-  }
+      self.setState({ optimizedData });
+    });
+  };
 
   private onScrollToSection = (item: ISectionData, sectionIndex: number) => {
     if (!this.sectionList) {
-      return
+      return;
     }
 
     if (item?.index === null) {
-      this.setState({ alphabetSelectedIndex: sectionIndex })
-      this.setState({ selectedLetter: item?.title })
+      this.setState({ alphabetSelectedIndex: sectionIndex });
+      this.setState({ selectedLetter: item?.title });
     } else {
       this.sectionList.scrollToLocation({
         sectionIndex: item?.index ?? 0,
         itemIndex: 0,
         animated: false,
-      })
-      this.setState({ alphabetSelectedIndex: sectionIndex })
-      this.setState({ selectedLetter: item?.title })
+      });
+      this.setState({ alphabetSelectedIndex: sectionIndex });
+      this.setState({ selectedLetter: item?.title });
     }
-  }
+  };
 
   private onSetSectionListRef = (sectionList: SectionList) => {
     if (!sectionList) {
-      return
+      return;
     }
 
-    this.sectionList = sectionList
-  }
+    this.sectionList = sectionList;
+  };
 
   private onRenderItem = ({ item }: { item: IData }) => {
-    const { renderItem } = this.props
+    const { renderItem } = this.props;
 
     if (renderItem) {
-      return renderItem(item)
+      return renderItem(item);
     }
 
     return (
       <View style={styles.listItemContainer}>
         <Text style={styles.listItemLabel}>{item.value}</Text>
       </View>
-    )
-  }
+    );
+  };
 
-  private onRenderSectionHeader = ({
-    section,
-  }: {
-    section: SectionListData<IData>
-  }) => {
-    if (!section?.data?.length) return null
+  private onRenderSectionHeader = ({ section }: { section: SectionListData<IData> }) => {
+    if (!section?.data?.length) return null;
 
-    const { renderSectionHeader } = this.props
+    const { renderSectionHeader } = this.props;
 
     if (renderSectionHeader) {
-      return renderSectionHeader(section)
+      return renderSectionHeader(section);
     }
 
     return (
       <View style={styles.sectionHeaderContainer}>
         <Text style={styles.sectionHeaderLabel}>{section.title}</Text>
       </View>
-    )
-  }
+    );
+  };
 
-  private _onViewableItemsChanged = ({ viewableItems, changed }) => {
-    const _self = this
+  private _onViewableItemsChanged = ({ viewableItems }: any) => {
+    const _self = this;
     if ((viewableItems?.length ?? 0) > 0) {
-      const selectedLetter = viewableItems[0]?.item?.title
-      _self.setState({ selectedLetter }), 300
+      const selectedLetter = viewableItems[0]?.item?.title;
+      _self.setState({ selectedLetter }), 300;
     }
-  }
+  };
 
   private viewabilityConfig = {
     waitForInteraction: true,
     viewAreaCoveragePercentThreshold: 95,
-  }
+  };
 
   render() {
-    const { selectedLetter, optimizedData } = this.state
+    const { selectedLetter, optimizedData } = this.state;
     return (
       <View style={[styles.container, this.props.style]}>
         <SectionList
@@ -187,6 +163,6 @@ export default class AlphabetList extends React.PureComponent<IAlphabetListProps
           selectedLetterItemStyle={this.props.selectedLetterItemStyle}
         />
       </View>
-    )
+    );
   }
 }
